f498437d117ed983993b047b3d4278f42acab192
d4rkp0rt <andy@d4rkp0rt.co.uk>
2021-08-31 13:47:54 +0100

    Base code for the TCP server

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import threading
 bind_ip = "0.0.0.0"
 bind_port = 9999
 
-server = socket.socket(bind_ip, bind_port)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((bind_ip, bind_port))
 
 server.listen(5)
 
